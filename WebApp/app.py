ee6462033d061c3b278c28c7af506636b03c7b63
tehcbing <16199244+tehcbing@users.noreply.github.com>
2022-02-22 22:49:14 +0800

    Add dashboard with rough layout

--- WebApp/app.py
+++ WebApp/app.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def index():
     return render_template("index.html")
 
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == '__main__':
     app.run()
